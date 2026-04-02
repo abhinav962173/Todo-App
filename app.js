ae952c267b5b6476c0a01fe288a32540e93612dc
Abhinav <dubeyabhinav2022@gmail.com>
2026-04-02 10:30:51 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+let btn = document.querySelector('button');
+let ul = document.querySelector('ul');
+let inp = document.querySelector('#inp');
+
+btn.addEventListener("click", function(){
+    let item = document.createElement('li');
+    item.innerText = inp.value;
+
+    let delBtn = document.createElement('button');
+    delBtn.innerText = "Delete";
+    delBtn.classList.add("delete");
+
+    ul.appendChild(item);
+    item.appendChild(delBtn);
+    inp.value = "";
+})
+
+ul.addEventListener("click", function(event){
+    if(event.target.nodeName === "BUTTON"){
+        let listItem = event.target.parentElement
+        listItem.remove();
+        console.log("Element deleted");
+    }
+})
+
