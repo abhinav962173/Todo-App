b64a08847bed1f68291ea4fc6a88c7a6a6c50045
Abhinav <dubeyabhinav2022@gmail.com>
2026-06-07 22:24:39 +0530

    Update Todo App

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-let btn = document.querySelector('button');
-let ul = document.querySelector('ul');
-let inp = document.querySelector('#inp');
+let inp = document.querySelector("#text");
+let div = document.querySelector("div");
+let ul = document.querySelector("ul");
+let btn = document.querySelector("button");
 
 btn.addEventListener("click", function(){
-    let item = document.createElement('li');
+    if(inp.value.trim() === ""){
+        return;
+    }
+
+    let item = document.createElement("li");
     item.innerText = inp.value;
 
-    let delBtn = document.createElement('button');
-    delBtn.innerText = "Delete";
-    delBtn.classList.add("delete");
+    let delbtn = document.createElement("button");
+    delbtn.innerText = "delete";
+    delbtn.classList.add("delete");
 
+    item.appendChild(delbtn);
     ul.appendChild(item);
-    item.appendChild(delBtn);
     inp.value = "";
-})
+});
+
 
 ul.addEventListener("click", function(event){
-    if(event.target.nodeName === "BUTTON"){
-        let listItem = event.target.parentElement
+    if(event.target.nodeName == "BUTTON"){
+        let listItem = (event.target.parentElement);
         listItem.remove();
-        console.log("Element deleted");
+        console.log("delete");
     }
 })
-
